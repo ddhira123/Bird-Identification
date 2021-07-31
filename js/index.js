d5e2137616ed2375fa0ee5689490c8a879b617a8
ddhira123 <dhana_dhira@apps.ipb.ac.id>
2021-07-31 10:08:13 +0700

    Some UI Changes

--- js/index.js
+++ js/index.js
@@ -8,12 +8,10 @@ function initDisplayFunctional() {
 		$("#paint").css({ width: "30vw", height: "30vw" });
 		$("#canvas").css({ width: "30vw", height: "30vw" });
 		$("#uploaded-img").css({ width: "30vw", height: "30vw" });
-		$("button").css({ "font-size": "30px" });
 	} else {
 		$("#paint").css({ width: "224px", height: "280px" });
 		$("#canvas").css({ width: "224px", height: "224px" });
 		$("#uploaded-img").css({ width: "224px", height: "224px" });
-		$("button").css({ "font-size": "22px" });
 	}
 
 	$("#clear").click(function () {
@@ -29,10 +27,13 @@ async function initPrediction() {
 	document.querySelector('input[type="file"]').addEventListener("change", function () {
 		if (this.files && this.files[0]) {
 			var img = document.getElementById("uploaded-img");
+			var noImg = document.getElementById("no-image");
 			img.onload = () => {
 				URL.revokeObjectURL(img.src); // no longer needed, free memory
 			};
 			img.src = URL.createObjectURL(this.files[0]);
+			img.style.display = "block";
+			noImg.style.display = "none";
 		}
 	});
 }
